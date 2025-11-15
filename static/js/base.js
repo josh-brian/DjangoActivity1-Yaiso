6f9bcd70123ade0247cdcef079e0a79ef2c3d8bd
josh-brian <brianjosh023@gmail.com>
2025-11-15 23:12:20 +0800

    commit

--- static/js/base.js
+++ static/js/base.js
@@ -1,14 +1,14 @@
 console.log("Javascript here");
 
-document.addEventListener("DOMContentLoaded", function () {
-    const greeting = document.getElementById("greeting");
-
-    if (greeting) {
-        const userName = prompt("Hi there! What’s your name?");
-        if (userName && userName.trim() !== "") {
-            greeting.textContent = `Hello, ${userName}! `;
-        } else {
-            greeting.textContent = "Hello there! ";
-        }
-    }
-});
+//document.addEventListener("DOMContentLoaded", function () {
+//    const greeting = document.getElementById("greeting");
+//
+//    if (greeting) {
+//        const userName = prompt("Hi there! What’s your name?");
+//        if (userName && userName.trim() !== "") {
+//            greeting.textContent = `Hello, ${userName}! `;
+//        } else {
+//            greeting.textContent = "Hello there! ";
+//        }
+//    }
+//});
